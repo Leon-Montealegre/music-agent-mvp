074441309c28a09a31a37da876c06869c74e506d
Leon-Montealegre <mathias.berthelemot@gmail.com>
2026-03-19 00:50:51 +0100

    register

--- frontend/src/middleware.js
+++ frontend/src/middleware.js
@@ -30,6 +30,6 @@ export default withAuth({
 
 export const config = {
   matcher: [
-    '/((?!login|api/auth|_next/static|_next/image|favicon.ico|logo.png).*)',
+    '/((?!login|register|api/auth|_next/static|_next/image|favicon.ico|logo.png).*)',
   ],
 }
